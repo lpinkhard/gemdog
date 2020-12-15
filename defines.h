76ff7b2101ced6e4ffb8a4e322876813d0aa97db
Lionel Pinkhard <lionelpinkhard@me.com>
2020-12-15 19:20:55 +0200

    Initial commit

---
+++ defines.h
@@ -0,0 +1,13 @@
+/* Allegro datafile object indexes, produced by dat v4.4.2, MinGW32 */
+/* Datafile: game.dat */
+/* Date: Sun Jan 27 20:00:22 2019 */
+/* Do not hand edit! */
+
+#define DIE_WAV                          0        /* SAMP */
+#define JUMP_WAV                         1        /* SAMP */
+#define LOGO_BMP                         2        /* BMP  */
+#define MUSIC_MID                        3        /* MIDI */
+#define PLANT_BMP                        4        /* BMP  */
+#define PLAYER_BMP                       5        /* BMP  */
+#define WIN_WAV                          6        /* SAMP */
+
